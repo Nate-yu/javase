15489eade18210543d746afc7400a592ea865a6a
Nate-yu <2242936124@qq.com>
2025-03-03 20:38:33 +0800

    方法

---
+++ src/main/java/method/VariableLengthArgument.java
@@ -0,0 +1,18 @@
+package method;
+
+public class VariableLengthArgument {
+    public static void printVariable(String... args) {
+        for (String s : args) {
+            System.out.println(s);
+        }
+    }
+
+    public static void printVariable(String arg1, String arg2) {
+        System.out.println(arg1 + arg2);
+    }
+
+    public static void main(String[] args) {
+        printVariable("a", "b");
+        printVariable("a","b","c","d");
+    }
+}
